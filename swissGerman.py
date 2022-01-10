04d7aad06f2f53ff1a7373dac575519578dcb755
Jacqueline Esther Hoeun Yu <togom11@g.skku.edu>
2022-01-11 08:41:28 +0900

    Create swissGerman.py

---
+++ swissGerman.py
@@ -0,0 +1,151 @@
+# //comment 
+
+
+# Swiss German Dictionary (alphabetically ordered)
+
+
+# AAabig (evening)aacho (to arrive)aadüte (to hint at sth) also: aatöneAakunft (arrival)Aaruef (phone call)aatöne (to hint) also: adüüteabartig (kinky)abegheie (to fall from something)Abflug (takeoff)abflüge (to takeoff)abhaue (to crop or to bunk)Abreis (departure)abreise (to depart)abverheit (failed)abverreckt (totally failed)Ade, Adjö (formal goodbye)Advent (advent, season before Christmas)Adventschranz (advent wreath)Adventskalender (Christmas calendar)Adventsmärt (advent market)Agända (diary)Alles Guete zum Geburi! (Happy Birthday!) also: Häppi Börsdei!alpott (again and again)amigs (sometimes)ängstirnig (narrow-minded) Anke (butter) also: Butteraper (snow free)
+
+# Arschbombe (cannonball, jumping butt first into the pool)
+
+# Ätti (dad in Bernese dialect)äuä (fat change!, come on!)Auto (car)autofahre (to drive a car, to ride a car)autostöpple (to hitchhike, lit. to stop a car)äxgüsi (from french "excusez", sorry)
+# BBaby (baby) also: Bébé, Buschi, Poppi
+
+# Badchappe (bathing cap)
+
+# Badchleid (bathing suit for women)
+
+# bade (to take a bath)Badefeerie (beach vacation)bädele (to swim, to play in the water)
+
+# Badhose (bathing trunks)
+
+# Badi (public pool) also: Schwümmbi
+
+# Badtüechli (towel)
+
+# Balkonie (staycation, lit. imaginary land on your balcony)Bäredräck (licorice)Bébé (baby) also: Baby, BuschiBerg (mountain)Beschprächig (meeting)Bett (bed)Bettfläsche (hot water bottle)Bettmümpfeli (bedtime snack)Bewärbig (application)bewundere (to admire)Bibeli (chick, pimple)
+# Binggis (children)
+# bisle (to pee) also: brünzlebiwakiere (to bivouac)bizzeli (a little bit)Blätter - leavesBleischtift (pencil)
+# Blödian (idiot) also: Tubelblöterle (to lollygag)Blueme (flower)Blüemli (little flower)blüttle (walk around undressed)Böllä (ball, onion (only in Zurich dialect))Bratwurscht (bratwurst)briegge (to cry) also: greine, gränne, hüüle, brüeleBrüeder (brother)Brüederli (little brother)brüele (to cry) also: greine, gränne, hüüle, brieggeBrünneli (washbasin, sink) also: Lawabo
+# brünzle (to pee) also: bislebruuchbar (useful)bsinne (to remember)bsoffe (drunk)Bsoffene (a drunk)Bsuecher (visitor)büätzä (to work) also: chrampfe, schaffeBüätzer (worker)bueche (to book)Buggel (bump)Buggeli (little bumps, bumps on a ski slope)Buggelpischte (mogul slope)Büro (office)Bürzi (chignon) also: Hüppi, PfürziBus (bus)Buschi (baby) also: Bébé, BabyBuschiwägeli (pram)busfahre (to ride a bus, to drive a bus)Büsi (cat) also: Busle, ChatzBusle (cat) also: Büsi, ChatzBütschgi ((apple) core) also: Groibschi, GigetschiBuuch (belly)Buuchweh (tummy ache)Buur (farmer)Buurehof (farm)Buurehof-Feerie (vacation on a farm)Ccampiere (to camp out)Campingplatz (campground) also: ZältplatzChalet (chalet)chalt (cold)Chälti (coldness)Chappe (touque, hat)Charte (map, card)Chäs (cheese)Chaschte (wardrobe, strong and heavy man)Chäschüechli (cheese tart)Chaugummi (bubble gum) also: CheutschgiCheib (bloke)chere (to turn)Cherze (candle)Cherzezieh (making candles)Cheutschgi (chewing gum) also: Chaugummi
+# chiischtrig (to be hoarse)Chilbi (fun fair, annual fair)Chind (child, children)Chindergeburi (child's birthday)Chlapf (smack)Chleider azieh (to put on clothes) also: sich aalegechlöpfe (to bang)
+# Chlöpfer (a Cervelat, a typical Swiss sausage)
+# chlüübe (to pinch)Chnebel (large piece of wood)chneble (to gag someone)choche (to cook)Chochtopf (cooking pot)Chopf (head)Chopfhörer (head phone)
+
+# Chöpfler (dive with head first) also: Schpiessli
+
+# Chopfweh (headache) also: Grindweh
+# chrable (to crawl)
+# chräble (to scratch)Chrälleli (beads)chrampfe (to work hard) Chriesi (cherry, cherries)Chriesischtei (cherry stone)Chrippe (manger, day care center)Chrischtbaum (Christmas tree)Chrischtchindli (Christ child)Chrone (crown)Chrotepösche (dandelion) also: SoibluemeChrüsimüsi (Chaos) also: DurenandChuchi (kitchen)Chuchichäschtli (kitchen cupboard)Chuchischublade (cupboard drawer) Chue (cow)Chueche (cake1)Chueflade (cowpat)Chuehnagel (frostbite on finger or toes, lit. cow nail)Chugelschriiber (ballpen) also: ChugiChugi (ballpen) also: Chugelschriiber
+# Chugele (globe, ball)
+# Chugelibahn (marble run)
+# chugele (to roll)Chum gli hei! (come home soon!)Chündigung (letter of dismissal)Chunnsch mit mir go Znacht ässe? (Will you go out to dinner with me?)Chürbis - pumpkinchüschele (to whisper in someone's ear)chützele (to tickle)chützelig (ticklish)chuum (unlikely, improbable)Cou-Cousin (male second cousin)Cou-Cousine (female second cousin)Cousin (male cousin)Cousine (female cousin)Dd'Hose ahaa (to wear the pants, e.g. to be in charge)dinne (inside)dischtanziert (aloof) Donnschtig (Thursday)driigheie (to fall for sth, to fall into sth)Du bisch de Beschti! (you're the best (for men))Du bisch di Beschti! (you're the best (for women))Du gsehsch mega hübsch us. (you look so pretty)Du machsch mi glücklich (you make me happy)Dubel (idiot) also: IdiotDurenand (chaos) also: Chrüsimüsodurenand sii (to be messy, confused)dusse (outside)EEgoischt (egoist)eh (anyway)Eichörnli (squirrel)Eier färbe (to color eggs, e.g. by cooking them in dye or drawing on them)Eier sueche (to look for the eggs)Eiertätsch (egg bump, i.e. an Easter game involving eggs)eimal (once)Eimal Kaffi bittschön (One coffee please)Eltere (parents)En Guete! (enjoy your meal, bon appetit)En Huffe Gäld (a pile of money)En Schöne (have a nice day)En schöne Valentinstag (happy valentinesday)Erdnüssli (peanut, typical gift from Santa)Erläbnis (adventure, experience)erschte Auguscht (1st of August = Switzerland's national day)erstuunlich (astounding)Es fägt (it's fun) Es guets Nöis (happy new year)Es menschelet (there are interpersonal problems)Es schöns Wucheänd (have a nice weekend)Eus gohts guet (we're all right)FFähli (fur)Familie (family)farbig - colorfulFäscht (party)fasle (to maunder, to babble)
+
+# Faul (foul)
+
+# Feerie (holiday, vacation)Feschtplatte (hard disk)figuretle (busywork)Finke (slipper)Fisimatänte mache (make a fuss about sth. when it's not really necessary)Fitze (Santa's stick that he uses to hit bad behaving children)
+
+# Flangge (cross pass in soccer)
+
+# Flitterwuche (honey moon)Flughafe (airport)Flugi (airplane) also: FlugzüügFlugzüüg (airplane) also: FlugiFluug (flight)Fluugangscht (fear of flying)flüüge (to fly)Fluughafe (airport)fötele (to take photos)Föteli (photo) also: FotiFoti (photo) also: FöteliFötzeli (scrap of paper)
+# Fotzelschnitte (lit. slice of paper scraps, French Toast)Frau (woman, wife)Frauefurz (firecracker, lit. A woman's fart)Friitig (friday)Früehlig (spring)Fründ (boyfriend, friend)Fründin (girlfriend, friend)früsch (fresh)fruschtriert (frustrated)Fudi (butt) also: FüdliFüdli (butt) also: Fudifüdliblutt (buttnaked) Füess (feet)
+
+# Fuessball (football, soccer, also lit. the soccer ball)
+
+# Fuessball spiele (to play soccer) also: tschutte
+
+# Fuessballplatz (soccer field) also: Tschuttiplatz
+
+# füessle (to touch each others feet)fulänze (to relax, to do nothing)Furz (fart)Füür (fire)füürle (to make a fire, to make a bonfire)Füürwärk (fireworks)Gga blüemle (to go pick flowers)ga bügle (to go to work)Garracho (high speed)Gascht (guest)Gäscht (guests)Geburi (birthday)genialschtens (awesome)Gepäck (luggage, baggage)gfroore (frozen)gfrüüre (to freeze)gfürchig (creepy)gheie (to fall)ghöre (to hear)Gigampfi (seesaw)Gigetschi (apple core) also: Bütschgi, Groibschi
+# gigele (to chuckle, to giggle)Glattiis (black ice)gleitschirmflüge (paragliding)glette (to iron)Glöggliböögg (silly, annoying person)Glugsi (hiccup) also: HitzgiGlunge (puddle)glüschtle (to look with envie) Gmüetlichkeit (coziness)Gnüss es! (have fun!)gnüsse (enjoy sth)Gnusswuche (week of indugence)goisse (to shriek)Gomfi (jam) also: GonfiGonfi (jam) also: GomfiGoof (brat)
+
+# Gool (goal)
+
+# Gooli (goalie)
+
+# göötsche (to play with water)Gopfriedstutz (darn it)Göppel (old car, old vehicle, old bicycle)
+
+# Gorner (corner kick)
+# göötsche (to play with water)
+
+# gränne (to cry, to weep) also: greine, hüüle, briegge, brüelegrau (grey)Grind (head)Grittibänz (bread roll in the shape of a man)Gromi (grandma) also: Grosi, GrosmamiGropi (grandpa) also: Grospapi, GrosättiGrosätti (grandpa) also: Gropi, GrospapiGroschind (grandchild, grandchildren)Groseltere (grandparents)Grosi (grandma) also: Gromi, GrosmamiGrosmami (grandma) also: Gromi, GrosiGrospapi (grandpa) also: Gropi, GrosättiGrossmami (grandma)Grossmeitli (granddaughter)Grübschi (apple core) also: Bütschgi, GigetschiGrücht (rumor)Grüezi (formal hello) also: Griezi, Grüessech Grüüsch (sound)Gschänkli (gift, gifts))Gschwüschterti (sibblings)gsund (healthy)Gsundheit! (Bless you! (after sneezing))guet (good)Guet Nacht (good night)Guete Aabig (good evening)Guete Morge (good morning)Guete Tag (good day)Guetzli (cookie, cookies)Güfeli (pin, needle pin)Gummistiefel (rain boots)gumpe (to jump)günschtig (cheap, good prize)Güsel (trash)Güselchübel (trash can)Gutsch (sip or spill)
+# güxle (to peek, to look)Gwitter (thunderstorm)HHaar (hair)Haargummeli (elastic hair band)Haarschnitt (haircut)habere (to eat)Hackbrätt (zither)häimelig (cozy)Halbpension (half board)Hallo (hello)Hampfle (handful)Händsche (gloves, mittens)Häppi Börsdei! (Happy Birthday!) also: Alles Guete zum Geburi!Härdöpfel (potato)Härdöpfelstock (mashed potatoes)heimlifeiss (to be more than can be seen at first sight (in a good or bad way))Hemli (shirt)Herbscht (fall, autumn)Herbschtferie (fall vacation)hiilegge (to fork out)hindersi fahre (to drive in reverse)Hochzig (wedding)Hoi (informal hello)Hoi zäme (informal hello to a group)Hoigümper (grasshopper)Hose (pants)Hosesack (pocket)Hostel (hostel)Hotel (hotel)Hotelzimmer (hotel room)Hudigägeler (Swiss folk music)Hueschte (to cough, a cough)Hülle (casing, covering)Hund (dog)Hündli (puppy)Hundsverlochete (party in the hills)Hüppi (chignon) also: Bürzi, Pfürzihüüle (to weep) also: greine, gränne, brüele, brieggeHuus (hous)Huusfrau (housewife)Huustür (door (of a house))IIch be ned Schwizer. (I am not Swiss (male))Ich be ned Schwizerin. (I am not Swiss (female))Ich be nur chli am umeluege. (I'm just looking.)Ich bi voll. (I'm full, I ate enough)Ich ha Milch gärn (I like milk)Ich has kapiert (I got it, I understood) also: Ich has verstande, Ich has tscheggtIch has tscheggt (I got it, I understood) also: Ich has kapiert, Ich has verstandeIch has verstande (I got it, I understood) also: Ich has kapiert, Ich has tscheggtIch liebe dich (I love you) Ich mag nüm. (I've had enough)Ich vermiss di. (I miss you)ID (Swiss identity card)Iglu (igloo)Iibruch (burglary)Iichaufe (shopping)iigschneit (to be snowed in, stuck in a place because of snow fall)iipacke (to bag something)iipacke (to bag something)
+
+# Iis (ice)
+
+# Iis (ice)iischlöfernd (soporific)iischalt (ice cold)iischneebele (to cover someone with snow)
+
+# Iishockey (hockey)
+
+# Iisräge (ice rain)Iiszapfe (icicle)iiu (sure)in Usgang goh (to go out)
+
+# inegumpe (dive in, jump in)
+
+# Jjödele (to yodel) also: jodlejodle (to yodel) also: jödeleJoggel (clumsy person, strange person, funny person)jöggele (to play table football) also: töggeleJöggelichaschte (table football) also: TöggelichaschteJugi (youth hostel)jung (young)jungi Lüt (young people)
+# KKaff (hicktown)Kafi (coffee)Karfriitig (Good Friday)
+# Karsumpel (stuff, junk)Kater (hangover, male cat)kei Ahnig (no idea)Koffer (suitcase)Kolleeg (friend)Kroki (sketch of a terrain)Kuckucksuhr (cuckoo clock)Kunscht (art)Künschtler (artist)Kunschtsammlig (art collection)LLampe (lamp)Lämpe (brawl)langi Unterhose (long johns)Langwili (boredom)Lätzli (bib)lauffe (to walk) Lawabo (washbasin, sink) also: BrünneliLawine (avalanche)Lawinegfohr (avalanche danger)lehre (to study, to teach)lerne (to study)Liebe Gruess (kind regards) also: Liebi GrüessLiebi (love)Liebi Grüess (kind regards) also: Liebe GruessLiecht (light) Liechterchetti (chain of lights)Liechtli (little light)Liegestuehl (deck chair)links (left, the left side)lisme (to knit)Löli (fool, student driver)Los mir zue! (listen to me!)lose (to listen)luege (to look)Luft (air)Luftibus (wanton person)Lunch (picnic lunch)lüüchte (to shine, to glow)Lüüchtstift (highlighter)Luune (mood)luunisch (moody)lüüte (ring)Lüütispiili (ring and run)MMaa (man, husband)
+# Mägerlimucki (a skinny child)Mami (mom) also: Mueti, Muttimampfe (to eat)Mandarinli (mandarine)Mäntig (Monday)Marroni (edible chestnuts)Marronitschtand (Marroni booth)Marroniverchäufer (Marroni vendor)Marzipaneili (marzipan eggs)Masseentlassig (collective dismissal)Meer (sea, ocean)mega fein (delicious)Menü (menu)messi (thank you, from french "merci") also: dankemischi maschi (gallimaufry)mitspile (to play along)Mittag (noon)Mittwuch (Wednesday)Morge (morning)Moscht (apple juice)moschte (to make applesauce)motze (to complain, to baa) also: mötzelemötzele (to complain, to baa) also: motzemüed si (to be tired)Mueter (mother)Mueti (mom) also: MamiNNäbel (fog)Näbelmeer (sea of fog)näblig (foggy)Nacht (night)Nachzügler (straggler)Nami (afternoon) also: NamitagNamitag (afternoon) also: NamiNatel (mobile phone)Natuur (nature, outdoors)Neffe (nephew)neutral (neutral) Nichte (niece)nigelnagelneu (brand spanking new)nonig (not yet)notiere (to annotate)nüüsse  (to sneeze)Oöbbe (approximately, sometimes)öbber (someone)öbber bewundere (to admire sb)öbber speiche (to kick sb)öbberem Honig ums Muul schmire (to butter sb up)öbbis (something)öbbis bewundere (to admire sth)öbbis ufgäh (to jettison sth, to give up sth)Ohregrübler (earwig)Öpfel (apple, apples)Oschtere (easter)Oschtereili (easter egg)Oschterhaas (easter bunny)Oschterhäsli (little easter bunny)Oschtermäntig (Easter Monday, a national holiday)Oschternäschtli (Easter nest,little basket you put eggs and bunnies in)PPapi (dad)Partner (partner, boyfriend, girlfriend)Pass (passport)Pass uf dich uf! (take care!) Passaschier (passenger)Passkontrolle (passport control)Pauschalreis (package tour, all-inclusive vacation)
+
+# Pedale (pedal)
+
+# penne (to sleep)Pfnüsel (cold, runny nose)Pfüderi (little rascal)Pfürzi (chignon) also: Bürzi, HüppiPilz (mushroom, mushrooms)
+
+# Pnö (tire)
+
+# Pögg (puck)
+# Poppi (baby) also: Baby, Bebé, Buschi
+
+# Poscht (mail, the post office)poschte (to shop)Poschtiwägeli (shopping cart)Proscht! (cheers!)
+
+# Proviant (provisions)
+
+# Prüefig (exam)Publikum (audience)Puff (mess, brothel)Qquängele (to be querulous)quängelig (querulous)Quartier (neighborhood)quirlig (lively)quätsche (to squeeze)
+# RRäbe - turnip, beetrootRäbeliechtli (beetroot lantern)Räbeliechtliumzug (beetroot lantern walk)rächts (right, the right side)rächtzittig (on time)Rad (wheel)Räge (rain)Rägeboge (rainbow)Rägemantel (rain coat)Rägeschirm (umbrella)Rägetropfe (rain drops)Rahm (cream) also: Nidle
+# Ränzler: (lit. belly-er or paunch-er; when you jump off the diving board and land smack on your tummy)Rappespalter (penny pincher)Reis (trip, journey)reise (to travel, to take a trip)Reisebüro (travel agency)Reisefieber (wanderlust)Reisefüdli (someone who travels a lot)Reisefüehrer (travel book, guide book)Reiseziel (destination)reläxe (to relax)Reschti (restaurant)Riitseili (swing) also: RiittiRiitti (swing) also: Riitseili
+# ring (easy, easily)Rootwii (red wine)Röschti (Swiss potato pancake)Röschtigrabe (imaginary line that separates the French and German speaking parts of Switzerland)Ross (horse)Rössli (small horse)Rossöpfel (road apple)Rucksack (backpack)Rüebli (carrot)Rüeblitorte (carrot cake)rüere (to stir, to throw)Rüerei (scrambled egg)
+
+# Rutschbahn (slide)
+
+# Ruum (room)Rüüm (rooms)SSAC Hütte (cottage of the Swiss Alpine Club)Sack (bag)säiche (to rain, to piss)Samichlaus (Santa Claus)Samschtig (Saturday)sändele (to play with sand)Schaal (scarf)schaffe (to work) also: chrampfeSchale (suit, coffee with milk, peel)Schiff (boat, ship)schifffahre (to ride a boat, to ride a ship)
+# Schii (ski)
+# Schiifahre (to ski)
+# Schiilift (ski lift)
+
+# Schiri (referee)
+
+# Schiss (fear)
+# Schisshaas (scaredy cat, lit. fear rabbit)Schläckzüg (sweets, candy) also: Zeltli, Täfeli
+
+# Schläger (hockey stick)
+
+# Schlamassel (mess)Schlirg (scrawl)Schlitte (sledge)
+
+# Schlittschueh (skate, ice skate)
+
+# schlittschüehle (to ice skate)
+
+# Schluck (sip)Schlückli (a little sip)schmöcke (to smell) schmuggle (to bootleg, to smuggle)Schmutzli (lit: dirty one, Santa's helper)schmuuse (to spoon with sb, to canoodle)schnaagge (to crawl)Schnee (snow)Schneeball (snow ball)Schneeball-Schlacht (snow ball fight)schneebele (to play in the snow)Schneeflocke (snowflake)Schneeflöckli (small snowflake)Schneeflöckli (snowflake)Schneefrau (snow woman)Schneemaa (snowman)Schneeschturm (snow storm)schneie (to snow)schnorchle (to snorkle)
+# Schnudderlumpe (lit. snot cloth, handkerchief)Schnuddernaase (running nose)Schnüggel (cuty)schnütze (to blow one's nose)schockierend (shocking)Schöggeler (someone who has an easy life)Schöggeli (small chocolates)Schoggi (chocolate)Schoggichueche (chocolate cake)Schoggihaas (chocolate bunny)Schoggischtängel (chocolate stick)Schöni Ferie (happy holidays)Schöni Wiehnacht (marry Xmas)Schoppe (baby milk bottle)
+# Schpelsache (toys, lit. playing things)
+
+# Schpiessli (dive with head first) also: Chöpfler
+
+# Schpinnhupele (cobweb)Schpoiz-Chnebel (recorder, flute)Schpriisse (splitter, splinter)schpringe (to run) also: seckle
+
+# Schprungbrätt (diving board)
+
+# Schprütze (injection, syringe)schprütze (to slosh, to besplatter)schriibe (to write)Schtäge (stairs) Schtange (bar, half a liter of beer)Schtärn (star) Schtärnli (little star)Schtell (job, place)Schtelleazeig (job advert)Schtern (star)Schtern vo Bethlehem (Chistmas star)schtier (broke, flat broke)Schtift (a pen or an apprentice)Schtraass (street)Schtrand (beach)Schtrasselampe (street lamp)schtrub (weird) Schtube (living room, sitting room)Schtuck (piece)Schtückli (morsel)schtudiere (to reflect or to study)Schtutz (Swiss Franc or a steep ascent) schüch (timid)Schuel (school)Schuelig (training)Schuum (foam)Schwager (brother-in-law)schwänze (to play hooky)Schwiegereltere (parents-in-law)Schwiegermueter (mother-in-law)Schwiegervatter (father-in-law)Schwiiz (Switzerland)Schwiizerdütsch (Swiss German)Schwö (sis)Schwögerin (sister-in-law)Schwöschter (sister)Schwöschterli (little sister)Schwümmbi (public pool) also: Badi
+
+# Schwümmbrüle (goggles)
+
+# schwümme (to swim)seckle (to run) also: schpringeSeegfrörni (freezing of a lake)Sehenswürdigkeit (tourist attraction)singe (to sing)Sitzig (meeting)Soiblueme (dandelion) also: ChrotepöscheSouvenir (souvenir)
+# Spazifizottel (stroll, walk)Stadtplan (city map)Stadtrundfahrt (sightseeing tour in a city)Ströfzgi (extra exercise)Summer (summer)Summerfeerie (summer vacation)sumpfig (boggy)Sunne (sun)Sunnebrand (sunburn)Sunnegräm (sunscreen)sünnele (to sun tan)Sunneschirm (sunshade)Sunntig (Sunday)süüferli (gradually, carefully)Suuser (young wine)Ttäderle (to sneak, to squeal, to tell on someone)Täderlisack (someone who tells on someone)Täfeli (candy, sweets) also: Zeltlitanke (to refuel)Tannebaum (fir tree)Tante (aunt)Täsche (bag)täubele (to outdare)Tee (tea)Teebüteli (tea bag)telefoniere (to make a phone call)tifig (quickly)töggele (to play table football) also: jöggeleTöggelichaschte (table football) also: Jöggelichaschtetönt guet (sounds good)tööne (to sound)
+
+# trampe (to pedal)
+
+# Truube (grape, grapes)Trüübeli (small grapes)tschalpe (to walk)tschättere (to judder)Tschüss (bye [to a friend])Tschüss zämme (bye all)Tschüssli (cheerio, bye)tschutte (to boot, to play soccer)
+
+# tschutte (to play soccer) also: Fuessball spiele
+
+# tschüttele (to play with a soccer ball, easy going soccer playing)
+
