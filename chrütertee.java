14917f3968f051cc02208f6fe06b63155d354717
Esther Hoeun Yu <togom11@g.skku.edu>
2022-02-01 09:12:52 +0900

    Update chrütertee.java
    chrütertee

--- chrütertee.java
+++ chrütertee.java
@@ -1,3 +1,68 @@
-1.  kau·der·welsch
-2. gruezi - 
-3. chrütertee - tea with herbs
+// 1.  kau·der·welsch
+// 2. gruezi - 
+// 3. chrütertee - tea with herbs
+
+
+dataType[] Wort;
+
+
+// declare an array
+double[] chrütertee;
+
+// allocate memory
+chrütertee = new double[10];
+
+double[] chrütertee = new double[10];
+
+//declare and initialize and array
+int[] chrütertee = {12, 4, 5, 2, 5};
+
+
+class Main {
+ public static void main(String[] args) {
+  
+   // create an array
+   int[] age = {12, 4, 5, 2, 5};
+
+   // access each array elements
+   System.out.println("Accessing Elements of Array:");
+   System.out.println("First Element: " + age[0]);
+   System.out.println("Second Element: " + age[1]);
+   System.out.println("Third Element: " + age[2]);
+   System.out.println("Fourth Element: " + age[3]);
+   System.out.println("Fifth Element: " + age[4]);
+ }
+}
+
+
+
+//for loop in Java 
+
+using chrütertee 
+
+for (chrütertee 1; chrütertee 2; chrütertee 3) {
+  // code block to be executed
+}
+  
+for (int chrütertee = 0; chrütertee < 5; chrütertee++) {
+  System.out.println(i);
+}
+
+for (int schnäbichetsche = 0; i <= 10; i = i + 2) {
+  System.out.println(i);
+}
+
+//syntax
+for (type variableName : arrayName) {
+  // code block to be executed
+}
+
+// 
+String[] habibi = {"schnäbichetsche ", "chrütertee", "wallah", "habibi"};
+for (String i : wallah) {
+  System.out.println(i);
+}
+
+//ETH: Eidgenössische Technische Hochschule Zürich
+
+wallah: 
