77309d7e5a7b7b6fe921a9287dec3b04c99d15d8
Esther Hoeun Yu <togom11@g.skku.edu>
2022-02-14 23:30:41 +0900

    Create SwissCultureShock.py

---
+++ SwissCultureShock.py
@@ -0,0 +1,32 @@
+Which of the following inventions was not made in Switzerland?
+1/8
+Shoe laces
+How many percent of the Alps are actually located in Switzerland, “land of the Alps”?
+2/8
+13%
+65%
+E
+Switzerland is popular as a movie set, in which of the following movies or series has Switzerland not been featured:
+3/8
+Game of Thrones
+E
+When was women’s suffrage (right to vote) officially introduced in Switzerland?
+4/8
+1971
+E
+Which of the following is not a law in Switzerland:
+5/8
+Dog owners must attend a course on how to train their dog.
+E
+Wearing a helmet is compulsory, when riding a bicycle.
+What is the official name of Switzerland?
+6/8
+Confoederatio Helvetica
+E
+How much chocolate do the Swiss eat on average per year:
+7/8
+10kg
+E
+Which of the following is not the name of a Swiss village, town or place:
+8/8
+Fondue
