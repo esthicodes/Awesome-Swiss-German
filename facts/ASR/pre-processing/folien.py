a4e2c51e446fd19f379655f5a2d9c81b83f13650
esthi <togom11@g.skku.edu>
2022-09-06 16:40:15 +0200

    Create folien.py

---
+++ facts/ASR/pre-processing/folien.py
@@ -0,0 +1,17 @@
+#checking if the file exists
+
+# Contributor: Aashish Agarwal
+
+import os
+import csv
+
+with open('/home/LTLab.lan/agarwal/german-speech-corpus/') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        print(row[0])
+        exists = os.path.isfile(row[0])
+        if exists:
+            temp=1
+        else:
+            print("Does not exist:" + row[0])
