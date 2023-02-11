b1a6fac29c0b6a2fda7e783d085a4ffe2710d259
hona <hoeuyu@student.ethz.ch>
2023-02-11 11:42:32 +0900

    Create teleskop.py

---
+++ datasets/zuerich/teleskop.py
@@ -0,0 +1,40 @@
+Ich lueg a *decki voller neonplastiksterne*
+S′fenster off, s einzig wo *chüehlt i dere summerwärmi*
+*S'strasseliecht vo duss wirft schattespiel ad zimmerwänd*
+Ein *gedankekreis*, *ich wüsst gern öbd au a mich denksch*
+
+Kei chance zum schlafe
+*Wo triebsch dich umenand?*
+Han gnueg vom warte
+*Hend viel z′viel abstand vonenand*
+
+I mim zimmer, *a mim fenster staht es teleskop vo dir*
+Lueg ich use, usem fenster, wünscht ich du *wärsch bi mir*
+*Du fehlsch da, suech überall, zoom mit em teleskop durs all*
+*I mim zimmer, a mim fenster staht es teleskop vo dir*
+
+*Uf mim iphone sind no alli *oisi* bilder*
+D'welt het *gstrahlet* dur de *rosaroti* filter
+
+S'wird nüt mit schlafe
+Min screen hilft dur d′nacht
+Han gnueg vom warte
+Seg mer wenn, *wenn hanis gschafft*?
+
+I mim zimmer, a mim fenster *staht* es teleskop vo dir
+Lueg ich use, usem fenster, *wünscht* ich du wärsch bi mir
+Du fehlsch da, *suech überall*, zoom mit em teleskop durs all
+I mim zimmer, a mim fenster staht es teleskop vo dir
+
+*Ich lueg a decki voller neonplastiksterne*
+*Irgendwo det uss starrsch du au id ferni*
+
+Mini auge *kämpfed*
+Langsam *werdeds* schwer
+Ich het mir *gwünscht* gha
+Dass du bi mir blibe *wärsch*
+
+<I mim zimmer, a mim fenster staht es teleskop vo dir
+Lueg ich use, usem fenster, wünscht ich du wärsch bi mir
+Du fehlsch da, suech überall, zoom mit em teleskop durs all
+I mim zimmer, a mim fenster *staht* es teleskop vo>
