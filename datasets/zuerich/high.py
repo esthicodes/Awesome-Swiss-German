de9b335c1651f803671f97ff6aaf703880d2f2f9
hona <hoeuyu@student.ethz.ch>
2023-02-11 11:55:27 +0900

    Create high.py

---
+++ datasets/zuerich/high.py
@@ -0,0 +1,54 @@
+Ich bin so high
+Ich bin so high
+
+Ich fang ah *schwitze*
+Als wär ich *uf entzug*
+*Mini händ am ziitre*
+*Wie bimene sturzflug*
+
+Ich *bruch nur e nase voll*
+Für en augeblick *rueh*
+*Chum gib mer en zug vo dir*
+Und ich fühl mich wieder guet
+
+Ich bin so high
+Fühl mich so high mit dir
+Ich bin so *ghypet*
+Fühl mich so ghypet mit dir
+Du weisch ich bruch dich die ganzi nacht
+Will du mich so süchtig machsch
+So high
+Ich bin so high
+
+*Min puls am rase*
+So viel adrenalin
+*Spiel mit offne charte*
+Ich bruche endorphin
+
+Ich han nur mit dir das gfühl
+*Als ob ich alles chan*
+*Mach mich uf de weg zu dir*
+*Dass ich das bald wieder han*
+
+Ich bin so high
+Fühl mich so high mit dir
+Ich bin so ghypet
+Fühl mich so ghypet mit dir
+Du weisch ich bruch dich die ganzi nacht
+Will du mich so süchtig machsch
+So high
+Ich bin so high
+
+High
+Ich bin so high
+
+Ich bin so high
+Fühl mich so high mit dir
+Ich bin so ghypet
+Fühl mich so ghypet mit dir
+Du weisch ich bruch dich die ganzi nacht
+Will du mich so süchtig machsch
+
+So high
+Ich bin so high
+
