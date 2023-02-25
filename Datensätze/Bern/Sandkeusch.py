a163c868c0242bb4ac3d8e7aa66c03a9f4601a97
hona <hoeuyu@student.ethz.ch>
2023-02-26 01:51:45 +0900

    Create Sandkeusch.py

---
+++ Datensätze/Bern/Sandkeusch.py
@@ -0,0 +1,34 @@
+Damals bin ich jede Tag ist Studio Gange,
+Gfange i dem Bunker Homie immer wenn ich schriibe
+Chömet die Erinnerige zrugg
+Ich weiss no ganz genau die ganzi Nacht, han immer welle blibe
+Am nächsten Tag go schaffe han immer verschlafe de Chef mich am hasse mer hend emmer müesse striite
+Doch han d Lehr bestande wärs nach mim Lehrer gange wär ich scho eher gange doch ich han grisse
+Trotzdem ben ich denn weg gange
+Min Weg gange
+Im Weg gstande send alli
+Doch han sie denn weggschlage
+Wie Ali, bechunnsch Herzrase vo Panik
+If ich em Benz fahr dur de Abig
+Nenn mich Xen Abi, mini Erfahrige zeigt
+Du machsch ja gern Party und du lengsch alli in scheiss
+Met dem wo t' hengsch amigs
+Zu dem wirsch Schlicht Homie
+Well du Wiis ziehsch Homie
+Besch de lieblings Homie du weisch
+Ich vermisse das Sandchaste-Lebe
+Sorgefrei da mit de Kids vom Nachbar am Henge
+Alles schiint guet doch es cha sich schlagartig wende
+Wien Schlag idie Fressi
+Mit 6i echo ahfange merke
+13i ganz Tag am schwänze
+Sie Herr Lehrernei es esch ned anstrengend zlerne
+Doch ich han ahfange Rappe
+D Jugend lebt nachem Yolo Motto bis sie met 18i sterbe
+Hoff dass sie nur vo mim Sound abhängig werdend
+Mal unabhängig werdet
+Anstatt nur unahständig umehenget
+Ständig einheitlich
+Ich ha das lebe so satt
+Nassdoch no chli lebe be lang nonig satt
+Ich muess ja fast glaube was am mäntig und ziistig bis am friitig i de ziitig so staht, drum fick uf de Staat!
