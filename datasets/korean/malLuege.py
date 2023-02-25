a129328992c65d78f0930d76bfd45b30cef0eaf1
hona <hoeuyu@student.ethz.ch>
2023-02-25 21:34:41 +0900

    Create malLuege.py

---
+++ datasets/korean/malLuege.py
@@ -0,0 +1,126 @@
+그것이 그녀가 의미하는 것입니다.
+가장 아름다운 바
+큰 꿀꺽 뮤트
+그리고 그녀에게 물어볼게
+"어떻게 누가, ois 2와 함께
+진정해, 둘 다
+일치하는 것은 친절합니다
+내꺼였어?"
+가끔 그녀는 거짓말을 해, 아마 내일 아니면 뭔가
+때때로 그녀는 거짓말을 하고 난 uf and devo
+가끔 그녀는 거짓말을 해, 아마 내일 아니면 뭔가
+그리고 그녀는 전혀 필요하지 않습니다
+당신은 나를 참조
+Uf 틴더가 다시 잡는다
+사진을 확인하세요
+게임하려면 오른쪽으로 스와이프하세요.
+나는 "당신은 모른다.
+빈 프리티그 이데 바
+헤쉬 당신은 gha를 의미합니까
+나는 선택했다
+당신은 좋은
+당신은 멋지다
+진정해, 둘 다
+너와 나
+유리 Wii
+좁은 동사의 Chönted
+세그가 내꺼였어?"
+가끔 그녀는 거짓말을 해, 아마 내일 아니면 뭔가
+때때로 그녀는 거짓말을 하고 난 uf and devo
+가끔 그녀는 거짓말을 해, 아마 내일 아니면 뭔가
+그리고 그녀는 전혀 필요하지 않습니다
+대학에서 또 다른 플랫 쉐어 파티 z'seebach
+오 젠장, 당신에게 유용합니다
+그녀는 기꺼이, 그리고 z'luut가 많이 있습니다
+Chum gömer use, 내게는 랑데뷰처럼 들리네
+D'wimpere klimperet, 그녀는 내가 무슨 뜻인지 묻습니다.
+그리고 난 정말 일시적인 전환이 없어
+가끔 난 거짓말을 해, 아마 내일 아니면 뭔가
+난 거짓말을 할 때 거짓말을 하고 난 uf와 devo
+가끔 난 거짓말을 해, 아마 내일 아니면 뭔가
+그리고 나는 그것을 전혀 필요로하지 않습니다
+
+Det staht sie
+Die schönst ade bar
+En grosse schluck muet
+Und denn frög ich sie mal
+"Wie wers, mit ois zwei
+Mal chille nur mir beid
+Macheds ois gmüetlich
+Was meinsch?"
+Mal luege het sie gseit, villicht morn oder so
+Mal luege het sie gseit und isch uf und devo
+Mal luege het sie gseit, villicht morn oder so
+Und siither isch vo ihre gar nüt meh cho
+Sie het mich
+Uf tinder wieder catcht
+Han d'bilder uscheckt
+Swipe nach rechts für de match
+Ich schrieb "weisch du no
+Am fritig ide bar
+Do hesch du gmeint gha
+Mir chönted mal
+Du bisch nice
+Du bisch geil
+Mal chille nur mir beid
+Du und ich
+Es glas wii
+Chönted im schmale verbi
+Seg was meinsch?"
+Mal luege het sie gseit, villicht morn oder so
+Mal luege het sie gseit und isch uf und devo
+Mal luege het sie gseit, villicht morn oder so
+Und siither isch vo ihre gar nüt meh cho
+Anere wg party z'seebach bim kolleg
+Ohni scheiss, usem nüt staht sie au det
+Sie seg so glangwillt, und da ine segs viel z'luut
+Chum gömer use, tönt für mich nach rendez-vous
+D'wimpere klimperet, sie frögt ja was ich mein?
+Und ich han würklich kein momentlang überleit
+Mal luege han ich gseit, villicht morn oder so
+Mal luege han ich gseit und bin uf und devo
+Mal luege han ich gseit, villicht morn oder so
+Und siither isch vo mir gar nüt meh cho
+
+That's what she stands for
+The nicest ade bar
+A big gulp muet
+And then I'll ask her
+"How who, with ois two
+Just chill, both of me
+Macheds ois cozy
+What do you mean?"
+Sometimes she lies, maybe tomorrow or something
+Sometimes she lies and I uf and devo
+Sometimes she lies, maybe tomorrow or something
+And there is no need for her at all
+she hates me
+Uf tinder catches again
+Check out the pictures
+Swipe right for the match
+I wrote "you don't know
+At the fritig ide bar
+Do you mean gha
+I chönted
+you are nice
+you are horny
+Just chill, both of me
+You and me
+It glass wii
+Chönted in the narrow verb
+Say what do you mean?"
+Sometimes she lies, maybe tomorrow or something
+Sometimes she lies and I uf and devo
+Sometimes she lies, maybe tomorrow or something
+And there is no need for her at all
+Another flat share party z'seebach at college
+Oh no shit, it's useful for you
+She's so willing, and there's a lot of z'luut
+Chum gömer use, sounds like a rendez-vous to me
+D'wimpere klimperet, she asks what I mean?
+And I really don't have a momentary transition
+Sometimes I'm lying, maybe tomorrow or something
+I'm lying when I'm lying and I'm uf and devo
+Sometimes I'm lying, maybe tomorrow or something
+And then I don't need it at all
+
